15add334424de678d8dd22eed41580c2f38c15b2
majidimtiaz <majid@majidimtiaz.com>
2022-01-18 13:38:48 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-n3xtde
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-n3xtde)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <ul><ui>First item</ui></ul>
+      <ul><ui>Seond item</ui></ul>
+      <ul><ui>Third item</ui></ul>
+      <ul><ui>Fourth item</ui></ul>
+      <ul><ui>Fifth item</ui></ul>
+    </div>
+  );
+}
